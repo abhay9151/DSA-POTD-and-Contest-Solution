90f8e8d024964b49c12cbf8c1f03c198a3238c44
Abhay Pratap Singh <saurabhsingh99195@gmail.com>
2026-09-02 19:41:33 +0530

    Time: 82 ms (75.67%) | Memory: 237.4 MB (42.72%) - LeetSync

---
+++ 403-frog-jump/frog-jump.cpp
@@ -0,0 +1,31 @@
+class Solution {
+    int solve(vector<int>&stones,int k,int index,vector<vector<int>>&dp){
+        if(index==stones.size()-1){
+            return true;
+        }
+        if(dp[k][index]!=-1)return dp[k][index];
+        for(int jump=k-1;jump<=k+1;jump++){
+            if(jump<=0)continue;
+            int next=stones[index]+jump;
+            for(int i=index+1;i<stones.size();i++){
+                if(stones[i]==next){
+                    if(solve(stones,jump,i,dp)){
+                        return dp[k][index]=1;
+                    }
+                    else{
+                        break;
+                    }
+                }
+            }
+        }
+        return dp[k][index]=0;
+
+    }
+public:
+    bool canCross(vector<int>& stones) {
+        int n=stones.size();
+        if(stones[1] != 1)return false;
+        vector<vector<int>>dp(n,vector<int>(n,-1));
+        return solve(stones,1,1,dp);
+    }
+};
