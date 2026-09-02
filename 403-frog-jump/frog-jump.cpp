305fd3deb2b103eb3b44c20bbdd978307becacbb
Abhay Pratap Singh <saurabhsingh99195@gmail.com>
2026-09-02 19:45:01 +0530

    Time: 83 ms (75.08%) | Memory: 237.6 MB (41.30%) - LeetSync

--- 403-frog-jump/frog-jump.cpp
+++ 403-frog-jump/frog-jump.cpp
@@ -8,12 +8,13 @@ class Solution {
             if(jump<=0)continue;
             int next=stones[index]+jump;
             for(int i=index+1;i<stones.size();i++){
+                //The frog Successfully reached stone i.Now continue solving the problem from this new stones with jump as jumpsize
                 if(stones[i]==next){
                     if(solve(stones,jump,i,dp)){
-                        return dp[k][index]=1;
+                        return dp[k][index]=1;// return true->from this stone the frog eventually reached the final stone.
                     }
                     else{
-                        break;
+                        break;// I found the required next stone ,stop searching for the particular jump.
                     }
                 }
             }
