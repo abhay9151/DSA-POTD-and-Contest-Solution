3d28cd3937504993e5ab8269a86d65310980c990
Abhay Pratap Singh <saurabhsingh99195@gmail.com>
2026-09-09 00:31:14 +0530

    Time: 3 ms (37.83%) | Memory: 10.8 MB (8.84%) - LeetSync

--- 977-distinct-subsequences-ii/distinct-subsequences-ii.cpp
+++ 977-distinct-subsequences-ii/distinct-subsequences-ii.cpp
@@ -1,41 +1,36 @@
 class Solution {
-
-    int solve(string &s, int i, vector<int> &dp) {
-
-        if(i == 0)
-            return 1;   // empty subsequence
-
-        if(dp[i] != -1)
-            return dp[i];
-
-        // take + not_take
-        long long ans = 2LL * solve(s, i - 1, dp);
-
-        // find previous occurrence of s[i-1]
-        int j = i - 2;
-
-        while(j >= 0 && s[j] != s[i - 1])
-            j--;
-
-        // remove duplicate subsequences
-        if(j >= 0) {
-            ans -= solve(s, j, dp);
+public:
+    const int M = 1e9 + 7;
+    int dp[2001];
+    vector<int> prev;
+    // prev[n] = last position where this character was seen
+    int solve(int n) {
+        if(n == 0)return 1;   // empty subsequence
+        if(dp[n] != -1)return dp[n];
+        // take current + don't take current
+        long long total = 2LL * solve(n - 1);
+        // If current character appeared before,
+        // duplicate subsequences are created
+        if(prev[n] != 0) {
+            total -= solve(prev[n] - 1);
         }
-
-        ans = (ans + 1000000007) % 1000000007;
-
-        return dp[i] = ans;
+        total = (total % M + M) % M;
+        return dp[n] = total;
     }
 
-public:
-
     int distinctSubseqII(string s) {
-
-        int n = s.size();
-
-        vector<int> dp(n + 1, -1);
-
-        // remove empty subsequence
-        return (solve(s, n, dp) - 1 + 1000000007) % 1000000007;
+        int n = s.length();
+         memset(dp, -1, sizeof(dp));
+        prev.assign(n + 1, 0);
+        vector<int> lastSeen(26, 0);
+        // Find previous occurrence of every character
+        for(int i = 1; i <= n; i++) {
+        int idx = s[i - 1] - 'a';
+        prev[i] = lastSeen[idx];
+        lastSeen[idx] = i;
+        }
+        // solve(n) includes empty subsequence
+        // so subtract 1
+        return (solve(n) - 1 + M) % M;
     }
 };
