ccbe90e35ca22877b37768a0b6220db6f9896b54
Abhay Pratap Singh <saurabhsingh99195@gmail.com>
2026-09-11 23:25:49 +0530

    Time: 2 ms (85.08%) | Memory: 30.9 MB (66.84%) - LeetSync

--- 3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.cpp
+++ 3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.cpp
@@ -2,20 +2,53 @@ class Solution {
 public:
     int totalNumbers(vector<int>& digits) {
         int n=digits.size();
-        set<int>st;
+        set<int>ans;
+        vector<int>mp(10,0);
         for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                for(int k=0;k<n;k++){
-                    if(i==j || j==k || i==k){
-                        continue;
-                    }
-                    int digit=digits[i]*100+digits[j]*10+digits[k]*1;
-                    if(digit>=100 && digit%2==0){
-                        st.insert(digit);
-                    }
-                }
-            }
+            mp[digits[i]]++;
         }
-        return st.size();
+        for(int i=1;i<=9;i++){
+            if(mp[i]==0)continue;
+            mp[i]--;
+            for(int j=0;j<=9;j++){
+                if(mp[j]==0)continue;
+                mp[j]--;
+                for(int k=0;k<=8;k+=2){
+                if(mp[k]==0)continue;
+                mp[k]--;
+                int digit=i*100+j*10+k*1;
+                if(digit>=100 && digit%2==0){
+                        ans.insert(digit);
+                }
+                mp[k]++;
+                }
+            mp[j]++;
+            }
+        mp[i]++;
+        }
+        return ans.size();
     }
 };
+
+
+// class Solution {
+// public:
+//     int totalNumbers(vector<int>& digits) {
+//         int n=digits.size();
+//         set<int>st;
+//         for(int i=0;i<n;i++){
+//             for(int j=0;j<n;j++){
+//                 for(int k=0;k<n;k++){
+//                     if(i==j || j==k || i==k){
+//                         continue;
+//                     }
+//                     int digit=digits[i]*100+digits[j]*10+digits[k]*1;
+//                     if(digit>=100 && digit%2==0){
+//                         st.insert(digit);
+//                     }
+//                 }
+//             }
+//         }
+//         return st.size();
+//     }
+// };
